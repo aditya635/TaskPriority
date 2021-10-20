9b590a5cc80ebb42ddf7dc2b583232b6a0a96069
aditya635 <55912563+aditya635@users.noreply.github.com>
2021-10-20 23:18:24 +0530

    Final

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL="sqlite:///./task.db"
+SQLALCHEMY_DATABASE_URL=os.environ["SQLALCHEMY_DATABASE_URL"]
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
